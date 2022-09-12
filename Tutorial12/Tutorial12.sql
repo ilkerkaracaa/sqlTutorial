ba92989a5b05fb007e04223d68463ac5616409a8
İlker Karaca <101411579+ilkerkaracaa@users.noreply.github.com>
2022-09-12 14:04:04 +0300

    Create Tutorial12.sql

---
+++ Tutorial12/Tutorial12.sql
@@ -0,0 +1,10 @@
+--1
+SELECT COUNT(*) FROM film WHERE length > ( SELECT AVG(length) FROM film );
+--2
+SELECT COUNT(*) FROM film WHERE rental_rate = (SELECT MAX(rental_rate ) FROM film);
+--3
+SELECT * FROM film WHERE rental_rate = (SELECT MIN(rental_rate ) FROM film) 
+AND replacement_cost = (SELECT MIN(replacement_cost ) FROM film);
+--4
+SELECT customer_id, SUM(amount) FROM payment GROUP BY customer_id 
+ORDER BY SUM(amount) DESC;
